36a431dd73824e8881c3301b51887691e4ae63c1
vvvbbbcz <vvvbbbcz@163.com>
2021-07-08 00:02:13 +0800

    一整天就搞了这些逼玩意？

--- src/main/java/xyz/bzstudio/civilizationswars/block/BlockList.java
+++ src/main/java/xyz/bzstudio/civilizationswars/block/BlockList.java
@@ -5,7 +5,6 @@ import net.minecraft.block.Block;
 import net.minecraft.block.OreBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.material.MaterialColor;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import xyz.bzstudio.civilizationswars.CivilizationsWars;
--- src/main/java/xyz/bzstudio/civilizationswars/tileentity/ElectromagneticCatapultTileEntity.java
+++ src/main/java/xyz/bzstudio/civilizationswars/tileentity/ElectromagneticCatapultTileEntity.java
@@ -1,9 +1,28 @@
 package xyz.bzstudio.civilizationswars.tileentity;
 
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 public class ElectromagneticCatapultTileEntity extends TileEntity {
+	public int speed = 0;
 	public ElectromagneticCatapultTileEntity() {
 		super(TileEntityTypeList.ELECTROMAGNETIC_CATAPULT);
+	}
+
+	@Nonnull
+	@Override
+	public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+		return super.getCapability(cap, side);
+	}
+
+	@Nonnull
+	@Override
+	public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap) {
+		return super.getCapability(cap);
 	}
 }
